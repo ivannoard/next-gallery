e0534ae38176a4714f6d9a094cc3cda79d41aa4a
ivannoard <ivannovaa.24@gmail.com>
2023-08-15 10:35:28 +0700

    add masonry grid || add tailwind custom aspect || add nextconfig for image

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Gallery = () => {
@@ -40,54 +41,365 @@ const Gallery = () => {
             <h4 className="text-center tracking-[.5rem]">
               - Unveiling Imagination&apos;s Horizons -
             </h4>
-            <div className="grid grid-cols-12 gap-5 mt-10">
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+            <div class="gap-8 columns-4 mt-10">
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=1"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=2"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=3"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=4"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=5"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=6"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-vertical mb-6"
+                  src="https://picsum.photos/500/300?random=7"
+                />
               </div>
-              <div className="col-span-3 h-[500px] bg-white text-black relative">
-                <div className="absolute bottom-0 left-0 right-0 p-5 bg-blue-500 text-center">
-                  <h4 className="tracking-[.4rem]">Djakarta</h4>
-                  <p className="text-sm">15 photos</p>
-                </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=8"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=9"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=10"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=11"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=12"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=13"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=14"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=15"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=16"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-vertical mb-6"
+                  src="https://picsum.photos/500/300?random=17"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=18"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=19"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=20"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=21"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=22"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=23"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=24"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=25"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=26"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-vertical mb-6"
+                  src="https://picsum.photos/500/300?random=27"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-video mb-6"
+                  src="https://picsum.photos/500/300?random=28"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=29"
+                />
+              </div>
+              <div className="relative">
+                <div className="absolute bg-black bg-opacity-0 hover:bg-opacity-70 transition w-full h-full top-0 bottom-0 left-0 right-0 mx-auto"></div>
+
+                <Image
+                  alt="image-gallery"
+                  width="500"
+                  height="500"
+                  objectFit="cover"
+                  class="w-full aspect-square mb-6"
+                  src="https://picsum.photos/500/300?random=30"
+                />
               </div>
             </div>
           </div>
