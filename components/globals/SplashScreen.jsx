26419c8b407392f034fcf79c3c7667bafae6d97c
ivannoard <ivannovaa.24@gmail.com>
2023-08-16 12:53:46 +0700

    add responsive loader

--- components/globals/SplashScreen.jsx
+++ components/globals/SplashScreen.jsx
@@ -2,7 +2,9 @@ const SplashScreen = () => {
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
-        <h1 className="text-white tracking-[1rem]">LOADING. . .</h1>
+        <h1 className="text-white tracking-[1rem]">
+          LOADING<span className="block text-center">. . .</span>
+        </h1>
       </div>
     </>
   );
